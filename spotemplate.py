a261cc0032e27c0475bf9ae5bbb0d13b9f9c1b6b
Noah Walker <MegosAlpha@users.noreply.github.com>
2021-02-28 03:25:22 -0700

    Fixed SPO template to work as a module if needed

--- spotemplate.py
+++ spotemplate.py
@@ -8,6 +8,8 @@ osid = platform.system()
 
 # Base-64 Platform Dependent Code (Filled by SPO)
 # Easy to extend, just handling the major cases basically
+# Will consider adding Apple Silicon support once Rosetta stops working
+# (or if Rust supports it and I have time)
 win64code = ""
 win32code = ""
 maccode = ""
@@ -20,26 +22,34 @@ is_64bits = sys.maxsize > 2**32
 
 # Universal .exe just because the other OS's shouldn't care about extensions here
 # Less with statements necessary
-with open(name + ".exe", 'wb') as f:
-    if osid == "Windows":
-        if is_64bits or win32code == "":
-            f.write(base64.b64decode(win64code))
-        else:
-            f.write(base64.b64decode(win32code))
-    elif osid == "Darwin":
-        f.write(base64.b64decode(maccode))
-        os.chmod(name + ".exe", 0o744)
-    elif osid == "Linux":
-        f.write(base64.b64decode(linuxcode))
-        os.chmod(name + ".exe", 0o744)
+def writeexe():
+    with open(name + ".exe", 'wb') as f:
+        if osid == "Windows":
+            if is_64bits or win32code == "":
+                f.write(base64.b64decode(win64code))
+            else:
+                f.write(base64.b64decode(win32code))
+        elif osid == "Darwin":
+            f.write(base64.b64decode(maccode))
+            os.chmod(name + ".exe", 0o744)
+        elif osid == "Linux":
+            f.write(base64.b64decode(linuxcode))
+            os.chmod(name + ".exe", 0o744)
+
+def cleanup():
+    if os.path.exists(name + ".exe"):
+        os.remove(name + ".exe")
 
 # Execution step
 # Note that this is somewhat geared towards unit testing
 # To handle such a case, impl in the program robust cmdline parameter support for its functions.
 
 def run_with_params(params):
-    actual_program = subprocess.run([os.path.join(os.path.curdir, name+".exe")] + params, shell=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
-    return actual_program.stdout
+    writeexe()
+    actual_program = subprocess.run([os.path.join(os.path.curdir, name+".exe")] + params, text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+    ot = actual_program.stdout
+    cleanup()
+    return ot
 
 def run_normal():
     if len(sys.argv) > 1:
@@ -52,5 +62,3 @@ print(run_normal(), end='')
 
 # End user block
 
-if os.path.exists(name + ".exe"):
-    os.remove(name + ".exe")
